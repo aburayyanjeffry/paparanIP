d5024a4819cb2015bd9c4c0184ba8ce25cff517a
Mohammad Jeffry Bin Mat Johar <S54034@TM.MY>
2019-07-22 09:33:26 +0800

    Initial. Show IP of the PC

---
+++ PaparanIP.bat
@@ -0,0 +1,21 @@
+@echo off
+REM Author: Jeffry Johar 2019
+
+@echo Selamat Datang ke Paparan I.P. oleh GIT Telekom Malaysia 
+ 
+
+@echo.
+
+for /f "tokens=2 delims=:" %%i in ('ipconfig^|findstr "Address"') do set ip=%%i
+
+@echo I.P Address   :%ip%
+
+@echo.
+
+@echo Nama Komputer : %COMPUTERNAME%
+
+@echo.
+
+@echo Tekan mana-mana kekunci untuk keluar
+
+pause>NUL
